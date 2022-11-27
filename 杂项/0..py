610c7a22d2c9cc23c40a5ecae3b1e4c8118c1c21
liushuangxi98 <1604030114@qq.com>
2022-11-27 23:27:19 +0800

    Changes

--- 杂项/0..py
+++ 杂项/0..py
@@ -1,22 +1,18 @@
-import re
-import string
-
-while True:
-    n = int(input())
-    matrix_dict = {i: list(map(int, input().split(' '))) for i in string.ascii_uppercase[0:n]}
-    rule = input()
-    counts = 0
-    while True:
-        temp_replace = re.findall('\(\w\w\)', rule)
-        if temp_replace:
-            temp_replace = temp_replace[0]
-            A = matrix_dict.get(temp_replace[1:3][0])
-            B = matrix_dict.get(temp_replace[1:3][1])
-            new_matrix, count = [A[0], B[1]], A[0] * B[1] * A[1]
-            matrix_dict.update({temp_replace[1:3][0].lower():new_matrix})
-            counts+=count
-            rule = rule.replace(temp_replace,temp_replace[1:3][0].lower())
-        else:
-            break
-    print(counts)
-
+# rule = input()
+# s = input()
+# if i_num:=rule.count('*'):
+#     min_len=len(rule)-i_num
+#     max_len=max(s)
+# else:
+#     min_len = len(rule)
+# if rule.count('*') == 0 and len(rule)!=len(s):
+#     print(False)
+#     exit()
+# if rule.count('*') == 0 and len(rule)==len(s):
+#     for i in range(len(rule)):
+#         if rule[i] != s[i] and rule[i] != '?':
+#             print(False)
+#             exit()
+# # 有* 的
+for i in range(10000000):
+    print(i)
